9a14c5e54c3ef347e07b526f9c024dfdfa5cf890
Niranjana nayak <niranjannayak1234bmw@gmail.com>
2024-01-16 10:07:12 +0530

    cors config util code

--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ dotenv.config({path: './env'})
 
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("app give error ", error);
+    })
+
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+})
 
 
 
