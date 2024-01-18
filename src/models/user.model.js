625c1523706c40448195b9e587f72369ee691c69
Niranjana nayak <niranjannayak1234bmw@gmail.com>
2024-01-18 12:30:10 +0530

    user controller finished

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-import { Jwt } from "jsonwebtoken";
+import Jwt from "jsonwebtoken";
 import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
@@ -50,7 +50,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function(next) {
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -22,3 +22,4 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 }
 
+export {uploadOnCloudinary}
