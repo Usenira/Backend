bbc0467db28cf4203ee73a9c4244e48a220ecc73
Niranjana nayak <niranjannayak1234bmw@gmail.com>
2024-01-18 08:30:35 +0530

    register route

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { express } from "express"
+import express from 'express'
 import cors from 'cors'
 import cookieParser from "cookie-parser"
 
@@ -14,5 +14,11 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+
+//routes decleration
+app.use("/api/v1/users", userRouter)
 
 export {app}
